8e50581ba10251960797d7aaadeede16743ffc99
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-22 23:05:49 -0300

    Arreglos menores.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-karucha
-=======
+# Codename: karucha
 
 Un programa sencillo para hacer avatares en huayra.
 
-## Instalación
+## ¿Cómo ejecutarlo?
 
-		git clone https://github.com/hugoruscitti/karucha.git
-		npm install
-		sudo npm install bower -g
-		cd src
-		bower install
-		cd ../
-		make test_linux
+En GNU/Linux podrías ejecutar:
+
+    git clone https://github.com/hugoruscitti/karucha.git
+    make test_linux
+
+O bien en MAC-OSX:
+
+    git clone https://github.com/hugoruscitti/karucha.git
+    make test_mac
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var app = angular.module('app', ['mm.foundation']);
 
 $(document).ready(function() {
+			$(document).foundation();
 });
 
 app.controller('MainCtrl', function($scope) {
@@ -11,51 +12,51 @@ app.controller('MainCtrl', function($scope) {
 
 app.factory("Canvas", function() {
 	var Canvas = {}
-	
+
 	Canvas.canvas = new fabric.Canvas('canvas');
-	
+
 	Canvas.definir_imagen_de_fondo = function(ruta) {
 		var c = Canvas.canvas;
 		c.setBackgroundImage(ruta, c.renderAll.bind(c));
 	}
-	
+
 	Canvas.agregar_imagen = function(ruta) {
 		fabric.Image.fromURL(ruta, function(img) {
-			
+
 			img.set({
         left: fabric.util.getRandomInt(0, 600),
         top: fabric.util.getRandomInt(0, 500),
-      });	
-			
+      });
+
 			img.perPixelTargetFind = true;
 			img.targetFindTolerance = 4;
-			
+
 			// Tinte de color !
 			var filter = new fabric.Image.filters.Tint({
   			color: '#3513B0',
 				//color: 'rgba(53, 21, 176, 0.5)'
   			opacity: 0.5
 			});
-			
+
 			img.filters.push(filter);
 			img.applyFilters(Canvas.canvas.renderAll.bind(Canvas.canvas));
-			
-			
-			
-			
-			
+
+
+
+
+
   		Canvas.canvas.add(img);
 			//Canvas.canvas.centerObject(img);
 			console.log(Canvas.canvas.toSVG());
-			
+
 			console.log(Canvas.canvas.toDataURL({format: 'png'}));
-			
-			
-			
+
+
+
 		});
 	}
-	
-	
+
+
 	var text = new fabric.Text('Hello world', {
   left: 100,
   top: 100,
@@ -63,94 +64,94 @@ app.factory("Canvas", function() {
   angle: 15
 });
 Canvas.canvas.add(text);
-	
+
 	return Canvas;
-	
+
 });
-		
+
 app.controller('GaleriaCtrl', function($scope, Canvas) {
 	var path = 'partes/';
-	
+
 	$scope.data = {};
 	$scope.data.directorios = [];
-	
-	
+
+
 	$scope.selecciona_objeto = function(obj) {
 		Canvas.agregar_imagen(obj.src);
 	}
-	
+
 	function actualizar_galeria(ruta_directorio, objeto_directorio) {
 		var titulo = objeto_directorio.titulo;
 		var indice = -1;
-		
+
 		for (var i=0; i<$scope.data.directorios.length; i++) {
 			if ($scope.data.directorios[i].titulo === titulo) {
 				indice = i;
 			}
 		}
-		
+
 		fs.readdir(ruta_directorio, function(error, data) {
 			$scope.data.directorios[indice].objetos = [];
-			
+
 			for (var i=0; i<data.length; i++) {
 				var ruta = data[i];
-				
+
 				if (/\.svg$/.test(ruta)) {
 					var item = {src: ruta_directorio + '/' + ruta};
 					$scope.data.directorios[indice].objetos.push(item);
 				}
-				
+
 			}
-		
+
 			$scope.$apply();
 		});
-		
+
 		//console.log(indice, ruta_directorio);
 		//objeto_directorio.objetos.push({src: "partes/pelo/nariz_1.svg"});
 	}
-	
+
 	function actualizar_listado_directorios() {
-		
+
 		fs.readdir(path, function(error, data) {
 			$scope.data.directorios = [];
-			
+
 			for (var i=0; i<data.length; i++) {
 				var titulo = data[i];
 				var objeto_directorio = {titulo: titulo, active: false, objetos: []}
-				
+
 				if (/^\./.test(titulo))
 					continue;
-				
+
 				$scope.data.directorios.push(objeto_directorio);
 				actualizar_galeria(path + titulo, objeto_directorio);
-				
+
 				// Comienza a observar cambios en la caleria, si se agrega
 				// un archivo .svg actualiza el listado.
 				var ruta = path + titulo;
-				
+
 				//fs.watch(ruta, function() {
 				//	alert(ruta);
 				//});
-				
+
 				//function(event, filename) {
-					
+
 				//	console.log(ruta, filename);
 					//if (/\.svg$/.test(filename)) {
 					//	actualizar_galeria(ruta, objeto_directorio);
 					//}
 
 				//});
-				
+
 			}
-			
+
 			$scope.$apply();
 		});
 	}
-	
+
 	actualizar_listado_directorios();
-		
+
 	fs.watch(path, function() {
 		actualizar_listado_directorios();
 	});
-	
+
 });
