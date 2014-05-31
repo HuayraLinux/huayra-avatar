72c27ce9b8d40e6bab6a50444da53209886ca2c1
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-31 13:47:03 -0300

    Llevando el arreglo de fabric a un archivo diferente.

--- src/js/factories/CanvasFactory.js
+++ src/js/factories/CanvasFactory.js
@@ -5,29 +5,6 @@ var sel_Rect = undefined;
 
 
 
-/*
- * Previene el bug clásico que no nos permitía seleccionar objetos
- * que se solapaban.
- */
-fabric.util.object.extend(fabric.Canvas.prototype, {
-  _searchPossibleTargets: function(e) {
-
-    var target,
-    pointer = this.getPointer(e);
-
-    var i = this._objects.length;
-
-    while(i--) {
-      if (this._checkTarget(e, this._objects[i], pointer)){
-        this.relatedTarget = this._objects[i];
-        target = this._objects[i];
-        break;
-      }
-    }
-
-    return target;
-  }
-});
 
 
 app.factory("Canvas", function() {
